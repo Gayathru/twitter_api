05a2abf17600cfac65d5490be6ef8e25de611822
Gayathru <45422044+Gayathru@users.noreply.github.com>
2021-05-21 13:12:06 +0530

    code added

---
+++ twitter_api.py
@@ -0,0 +1,32 @@
+import tweepy
+import sys
+
+access_token="1395443487068233729-mPf88Fz2PT3BaDj7XG8ITvfgygmtwH"
+access_token_secret="EraRSZQSEN2t1QeO2NWK94t1jarsvVxfWWeqp9EGgbC69"
+
+api_key="X09UmwH4TO3N4lgJTBuSblYtp"
+api_key_secret="5PNGEXhm5XmeqtMKxmhoUca5rrSxMm38wgLIjJ6KP6JxPhve8O"
+
+auth=tweepy.OAuthHandler(consumer_key=api_key,consumer_secret=api_key_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api=tweepy.API(auth)
+
+print(api)
+
+india_woeid=23424848
+
+trend_result=api.trends_place(india_woeid)
+
+val=input("Enter a hashtag : ")
+
+for trend in trend_result[0]["trends"][:]:
+    if val==(trend["name"]):
+         
+        sys.stdout=open("index.html", "w")
+        print(trend["tweet_volume"])
+        sys.stdout.close()
+    
+
+
+
